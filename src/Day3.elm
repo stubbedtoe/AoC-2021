2adbc267b74d92d78fd57e663e9f01b3ff6f8cae
Andrew Healy <andrewhealy@deloitte.co.uk>
2021-12-12 14:59:38 +0000

    Day 3 part 2

--- src/Day3.elm
+++ src/Day3.elm
@@ -1,5 +1,6 @@
 module Day3 exposing (solution)
 
+import Array exposing (Array)
 import Day3input
 import Types exposing (Solution)
 
@@ -46,11 +47,14 @@ getGammaEpsilon column =
         sum =
             List.filterMap (\c -> String.toInt (String.fromChar c)) column
                 |> List.sum
+                |> toFloat
 
         half =
-            List.length column // 2
+            List.length column
+                |> toFloat
+                |> (\i -> i / 2)
     in
-    if sum > half then
+    if sum >= half then
         { gamma = "1", epsilon = "0" }
 
     else
@@ -92,41 +96,90 @@ part1 =
         |> gammaEpilsonToAnswer
 
 
+getLifeSupportRating : Int -> Int -> String
+getLifeSupportRating o2GeneratorRating co2ScrubberRating =
+    o2GeneratorRating
+        * co2ScrubberRating
+        |> String.fromInt
 
--- getOxegenGeneratorRating : List GammaEplison -> List (List Char) -> List Char
--- getOxegenGeneratorRating gammaEpsilons binaries =
---     case binaries of
---         [] ->
---             []
---         binary::[] ->
---             binary
---         _ ->
---             let newBinaries =
---                 List.filter
--- part2 : String
--- part2 =
---     let
---         asStringList =
---             String.lines Day3input.input
---         stringLength =
---             case asStringList of
---                 x :: _ ->
---                     String.length x
---                 [] ->
---                     0
---         asCharList =
---             String.join "" asStringList
---                 |> String.toList
---     in
---     List.range 0 (stringLength - 1)
---         |> List.map (\skip -> listFromArray asCharList stringLength skip)
---         |> List.map getGammaEpsilon
---         |> oneFromMany { gamma = "", epsilon = "" }
---         |> gammaEpilsonToAnswer
+
+getRating : (GammaEplison -> String) -> List (List Char) -> Int -> Maybe Int
+getRating which binaries index =
+    case binaries of
+        [] ->
+            Nothing
+
+        binary :: [] ->
+            String.fromList binary
+                |> binaryToInt
+                |> Just
+
+        binary :: rest ->
+            let
+                stringLength =
+                    List.length binary
+
+                asStringList =
+                    List.map String.fromList binaries
+
+                asCharList =
+                    String.join "" asStringList
+                        |> String.toList
+
+                gammaEpsilon : GammaEplison
+                gammaEpsilon =
+                    listFromArray asCharList stringLength index
+                        |> getGammaEpsilon
+
+                newBinaries : List (List Char)
+                newBinaries =
+                    List.filter
+                        (\b ->
+                            Array.fromList b
+                                |> (\a ->
+                                        case Array.get index a of
+                                            Just c ->
+                                                String.fromChar c == which gammaEpsilon
+
+                                            Nothing ->
+                                                False
+                                   )
+                        )
+                        binaries
+            in
+            case newBinaries of
+                bin :: [] ->
+                    String.fromList bin
+                        |> binaryToInt
+                        |> Just
+
+                _ ->
+                    getRating which newBinaries (index + 1)
+
+
+part2 : Maybe String
+part2 =
+    let
+        rows =
+            String.lines Day3input.input
+                |> List.map String.toList
+
+        o2GeneratorRating =
+            getRating .gamma rows 0
+
+        co2ScrubberRating =
+            getRating .epsilon rows 0
+    in
+    case ( o2GeneratorRating, co2ScrubberRating ) of
+        ( Just generator, Just scrubber ) ->
+            Just (getLifeSupportRating generator scrubber)
+
+        _ ->
+            Nothing
 
 
 solution : Solution
 solution =
     { part1 = Just part1
-    , part2 = Nothing
+    , part2 = part2
     }
